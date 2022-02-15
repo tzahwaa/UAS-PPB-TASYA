9d2a183e9a916558e385cd5a87ab764a94bad2b4
tzahwaa <tzahwa16@gmail.com>
2022-02-15 11:58:54 +0700

    UAS PPB

---
+++ app/src/main/java/tasya/example/uasppb/Main2Activity.kt
@@ -0,0 +1,12 @@
+package tasya.example.uasppb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
